ff3fce8fd111e5c294ed62a27729c0c0f9145d0c
AndrewSamoilov <andrew.smv94@gmail.com>
2026-01-14 16:44:20 +0200

    add solution for "Minimum Size Subarray Sum" problem

--- src/main/java/com/andrew/smv/two_pointers/leetcode/subarray_product_less_than_k/Solution.java
+++ src/main/java/com/andrew/smv/two_pointers/leetcode/subarray_product_less_than_k/Solution.java
@@ -3,18 +3,17 @@ package com.andrew.smv.two_pointers.leetcode.subarray_product_less_than_k;
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
 
+        if (k <= 1){
+            return 0;
+        }
+
         int ans = 0;
         int product = 1;
 
         int l = 0;
         for (int i = 0; i < nums.length; i++) {
 
-
-            if (product == 0) {
-                product = nums[i];
-            } else {
-                product *= nums[i];
-            }
+            product *= nums[i];
 
             while (product >= k && l < nums.length) {
                 product /= nums[l];
@@ -23,6 +22,6 @@ class Solution {
 
             ans += i - l + 1;
         }
-        return Math.max(ans, 0);
+        return ans;
     }
 }
